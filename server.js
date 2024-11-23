06d27b1c22cd2e38c0e54ba9328df694704e0926
Eryk Neri <eryk.ner@gmail.com>
2024-11-22 22:14:18 -0300

    Aula 2 Imersão dev

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descricao: "Uma foto test",
+        imagem: "https://placecats.com/mille/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Outro post legal",
+        imagem: "https://placecats.com/mille/300/150"
+    },
+    {
+        id: 3,
+        descricao: "Mais um post",
+        imagem: "https://placecats.com/mille/300/150"
+    },
+    {
+        id: 4,
+        descricao: "Novo post incrível",
+        imagem: "https://placecats.com/mille/300/150"
+    },
+    {
+        id: 5,
+        descricao: "Mais um post interessante",
+        imagem: "https://placecats.com/mille/300/150"
+    }
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Funcionado....");
 });
 
-app.get("/api",(req, rest) => {
-    rest.status(200).send("Status 200");
-})
+function buscarPostID(id){
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+}
+
+app.get("/posts/:id", (req, rest) => {
+    const index = buscarPostID(req.params.id);
+    rest.status(200).json(posts[index]);
+});
