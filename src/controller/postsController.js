f29522bb45292d13d99789e1270bff01f939d58c
Eryk Neri <eryk.ner@gmail.com>
2024-11-27 16:09:55 -0300

    Aula 4 Imersão dev

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -1,8 +1,36 @@
 // Importando o modelo de posts
-import getTodosPosts from "../models/postsModel.js";
+import  { getTodosPosts, criarPost } from "../models/postsModel.js";
+import fs from "fs"
 
 // Função para obter todos os posts
 export async function listarPosts(req, rest) {
     const posts = await getTodosPosts();
     rest.status(200).json(posts);
+}
+
+export async function NewPost(req, res) {
+    const novoPost = req.body;
+    try {
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+    } catch (error) {
+        console.log(erro.messagem);
+        res.status(500).json({"Erro":"falha na requisição"})
+    }
+}
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: "",
+    };
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+        fs.renameSync(req.file.path, imagemAtualizada);
+        res.status(200).json(postCriado);
+    } catch (error) {
+        console.log(erro.messagem);
+        res.status(500).json({"Erro":"falha na requisição"})
+    }
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,31 @@
 // Importando o express e as rotas dos posts
-import express from "express";
-import { listarPosts } from "../controller/postsController.js";
+import express from "express"; // Importa o framework Express.js para criar a aplicação web
+import { listarPosts, NewPost, uploadImagem } from "../controller/postsController.js"; // Importa as funções controladoras para posts (listarPosts, NewPost, uploadImagem) do arquivo postsController.js
+import multer from "multer"; // Importa o middleware multer para manipular uploads de arquivos
 
-const routes = (app) => {
-    app.use(express.json());
+const storage = multer.diskStorage({ // Define as configurações de armazenamento para uploads
+  destination: function (req, file, cb) { // Função para definir o diretório de destino dos uploads
+    cb(null, 'uploads/'); // Define o diretório 'uploads/' para armazenar os arquivos
+  },
+  filename: function (req, file, cb) { // Função para definir o nome do arquivo após o upload
+    cb(null, file.originalname); // Usa o nome original do arquivo para mantê-lo
+  }
+});
 
-    app.get("/posts", listarPosts);
+const upload = multer({ dest: "./uploads", storage }); // Cria uma instância do middleware multer com as configurações de armazenamento
+
+const routes = (app) => { // Função que define as rotas da aplicação
+  // Permite que o servidor interprete requisições com JSON
+  app.use(express.json()); // Habilita o middleware express.json para que o servidor possa entender dados enviados no formato JSON
+
+  // Define uma rota GET para "/posts":
+  app.get("/posts", listarPosts); // Define uma rota que responde a requisições GET para "/posts" utilizando a função listarPosts
+
+  // Define uma rota POST para "/posts": (corrigido para POST)
+  app.post("/posts", NewPost); // Define uma rota que responde a requisições POST para "/posts" utilizando a função NewPost
+
+  // Define uma rota POST para "/upload":
+  app.post("/upload", upload.single("imagem"), uploadImagem); // Define uma rota que responde a requisições POST para "/upload" utilizando o middleware upload.single("imagem") para manipular o arquivo enviado com o campo "imagem" e, em seguida, executa a função uploadImagem
 };
 
-export default routes;
+export default routes; // Exporta a função routes para ser utilizada em outros arquivos
